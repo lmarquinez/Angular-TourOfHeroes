7c39b09b9897cbd7e008bf389870357df8691c9c
Laura Marquínez <l.marquinez@lantek.com>
2023-03-07 15:49:24 +0100

    Display a list
    Summary:
    - The Tour of Heroes application displays a list of heroes with a detail view.
    - The user can select a hero and see that hero's details.
    - Used *ngFor to display a list.
    - Used *ngIf to conditionally include or exclude a block of HTML.
    - Can toggle a CSS style class with a class binding.

--- TourOfHeroes/src/app/components/heroes/heroes.component.ts
+++ TourOfHeroes/src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from '../../interfaces/hero.interface';
+import { HEROES } from '../../db/mock-heroes.db'
 
 @Component({
   selector: 'app-heroes',
@@ -8,9 +9,18 @@ import { Hero } from '../../interfaces/hero.interface';
 })
 export class HeroesComponent {
 
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-  
+  // hero: Hero = {
+  //   id: 1,
+  //   name: 'Windstorm'
+  // };
+
+  heroes = HEROES;
+
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+
 }
