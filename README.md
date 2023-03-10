4b7d85b00dc8c9c1925062e465a4d9bbd730e323
Laura Marquínez <l.marquinez@lantek.com>
2023-03-10 10:20:57 +0100

    Last default tests fixed

--- README.md
+++ README.md
@@ -1,21 +1,35 @@
 # Angular Training
+
 ## _Tour Of Heroes_
+
 https://angular.io/tutorial/tour-of-heroes
+
 ```sh
 cd TourOfHeroes
 npm i
 ng serve
 ```
+
 ## _Hero Form_
+
 https://angular.io/guide/forms
+
 ```sh
 cd HeroForm
 npm i
 ng serve
 ```
+
 ## _Testing with Jest on TourOfHeroes_
+
 ```sh
 cd TestingJest
 npm i
+jest
+```
+
+or with
+
+```sh
 npm run test:watch
 ```
--- TestingJest/tests/components/hero-detail.component.spec.ts
+++ TestingJest/tests/components/hero-detail.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeroDetailComponent } from '../../src/app/components/hero-detail/hero-detail.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
@@ -8,7 +9,7 @@ describe('HeroDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [HttpClientTestingModule, RouterTestingModule],
       declarations: [HeroDetailComponent],
     }).compileComponents();
 
